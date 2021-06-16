1346a53815e14ac3a16a800c16a73e2a95f5f61b
Mike A. Cedric <mike.a.cedric@ibm.com>
2021-06-17 02:20:15 +0500

    added readme and also edited appjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./stakater.png";
 import "./App.css";
 import React, { Component } from "react";
-var moment = require('moment'); 
+var moment = require("moment");
 
 class App extends React.Component {
   constructor(props) {
@@ -15,21 +15,20 @@ class App extends React.Component {
       .then((res) => this.setState({ apiResponse: res }));
   }
 
-   getCurrentTime() {
-    var currentdate = new Date();
-    var datetime =
-      " " +
-      currentdate.getDate() +
+  getCurrentTime() {
+    var date = new Date();
+    var dateStr =
+      ("00" + (date.getMonth() + 1)).slice(-2) +
       "/" +
-      (currentdate.getMonth() + 1) +
+      ("00" + date.getDate()).slice(-2) +
       "/" +
-      currentdate.getFullYear() +
+      date.getFullYear() +
       " " +
-      currentdate.getHours() +
+      ("00" + date.getHours()).slice(-2) +
       ":" +
-      currentdate.getMinutes(); 
-  
-    return datetime;
+      ("00" + date.getMinutes()).slice(-2);
+
+    return dateStr;
   }
 
   componentWillMount() {
@@ -38,41 +37,16 @@ class App extends React.Component {
 
   render() {
     return (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
- {this.getCurrentTime()} {this.state.apiResponse}
-              </p>
-  
-            </header>
-          </div>
-        );
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>
+            {this.getCurrentTime()} {this.state.apiResponse}
+          </p>
+        </header>
+      </div>
+    );
   }
 }
-
-
-
-// function App() {
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
